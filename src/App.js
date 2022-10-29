d5820b84d9f5d505fb0f7f886976df5796aed471
lenagatseva79 <lenakassem79@gmail.com>
2022-10-29 23:11:58 +0300

    Added bootsrtap

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/lena-gatseva-375802202/"
-          target="_blank"
-        >
-          Lena Gatseva
-        </a>{" "}
-        and it is{" "}
-        <a
-          href="https://github.com/lenagatseva79/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/lena-gatseva-375802202/"
+            target="_blank"
+          >
+            Lena Gatseva
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/lenagatseva79/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
