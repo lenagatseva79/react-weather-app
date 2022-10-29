e82a6bddbd45e987c68123a5b53bd1a6386246cb
lenagatseva79 <lenakassem79@gmail.com>
2022-10-29 22:53:28 +0300

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import react from "react";
+
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/lena-gatseva-375802202/"
+          target="_blank">
+          Lena Gatseva
+        </a>{" "}
+        and it is
+        <a
+          href="https://github.com/lenagatseva79/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
