c1d1fb1900e61c40d498f6630f6b5fe5dea25af1
lenagatseva79 <lenakassem79@gmail.com>
2022-11-05 00:26:30 +0200

    Added css
    colors, icon

--- src/Weather.js
+++ src/Weather.js
@@ -5,50 +5,51 @@ import "./Weather.css";
 export default function Weather() {
     return (
       <div className="Weather">
-        <div className="row">
-          <div className="col-4">
-            <form>
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    placeholder="Enter a city.."
-                    className="form-control"
-                  />
+          <div className="row row-border">
+            <div className="col-4 leftSide">
+              <form>
+                <div className="row">
+                  <div className="col-9">
+                    <input
+                      type="search"
+                      placeholder="Enter a city.."
+                      className="form-control"
+                    />
+                  </div>
+                  <div className="col-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-primary"
+                    />
+                  </div>
                 </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary"
-                  />
-                </div>
-              </div>
-            </form>
-            <h1>Odesa</h1>
-            <ul>
-              <img
-                src="images/cloud.png"
-                alt="Mostly cloudy"
-              />
-              <li>15°C</li>
-              <li>Mostly cloudy</li>
-              <li>Sunday 11:00</li>
-            </ul>
-          </div>
-          <div className="col-8">
-            <h4>This weeks weather</h4>
-            <h4>Today's highlights</h4>
-            <ul>
-              <li>Sunrise</li>
-              <li>Sunset</li>
-              <li>Feels like</li>
-              <li>Humidity</li>
-              <li>Wind Speed</li>
-              <li>Visibility</li>
-            </ul>
+              </form>
+              <ul>
+                <img
+                  src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/051/513/original/cloudy.png?1667595985"
+                  class="icon"
+                  alt="Mostly cloudy"
+                />
+                <h1>15°C</h1>
+                <li>Mostly cloudy</li>
+                <li>Sunday 11:00</li>
+                <li>📍Odesa</li>
+              </ul>
+            </div>
+            <div className="col-8 rightSide">
+              <h4>This weeks weather</h4>
+              <h4>Today's highlights for city</h4>
+              <ul>
+                <li>Sunrise</li>
+                <li>Sunset</li>
+                <li>Feels like</li>
+                <li>Humidity</li>
+                <li>Wind Speed</li>
+                <li>Visibility</li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
     );   
 } 
